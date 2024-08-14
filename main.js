29c6699f3eea93a635d24119c04bd7e237e68918
SteamCode <steamcodeyt@gmail.com>
2024-08-13 21:28:25 -0500

    Added basic increment button

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    alert("hello, world!")
+    var number = 0
+    var num_inc_btn_delta = 1
+
+    $('.num_inc_btn').click(function(){
+        increment_number(num_inc_btn_delta)
+    });
+
+    function increment_number(delta){
+        number += delta
+        $('.num_text').html("Number = " + number)
+        return number
+    }
 })
